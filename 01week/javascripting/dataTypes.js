8a48ec70a8ff28d57c1141612f077859e14d8be8
Ty Sweet <tysweet77@gmail.com>
2018-01-19 10:47:28 -0600

    added all functions to assignment

--- 01week/javascripting/dataTypes.js
+++ 01week/javascripting/dataTypes.js
@@ -1,25 +1,75 @@
 'use strict';
 console.log('here');
-//Write a JavaScript program that adds 2 numbers together.
-const sumOfTwoNumbers=(num1, mum2)=>{
-  return num1 + mum2
+// Write a JavaScript program to display the current day and time
+function newDate() {
+  return Date();
+};
+
+newDate();
+
+// Write a JavaScript program to convert a number to a string
+function numToString(num) {
+  return num.toString();
+};
+
+numToString(9);
+
+// Write a JavaScript program to convert a string to the number
+function stringToNum(str) {
+  return Number(str);
 }
 
-sumOfTwoNumbers(3,2);
+stringToNum('seven');
 
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+// Boolean
+// Null
+// Undefined
+// Number
+// NaN
+// String
+function whatType (arg1) {
+  return typeof arg1;
+};
 
-// Write a JavaScript program that runs only when 2 things are true.
+whatType(null);
 
-// function that recieves to items, if those two things are true.
-// methods: function, 2 arguments, if/ then statement
-
-function evaluatesToTrue(arg1, arg2) {
-  if(arg1 && arg2){
-    return 'both are true'
-  }else{
-    return 'nope'
-  }
+// Write a JavaScript program that adds 2 numbers together
+function addTwoNums(num1, num2) {
+  return num1 + num2;
 }
 
-evaluatesToTrue(4, 6);
-evaluatesToTrue(null, 6);
+addTwoNums(4, 3);
+
+// Write a JavaScript program that runs only when 2 things are true
+function twoAreTrue(arg1, arg2) {
+  if(arg1 && arg2) {
+    return 'Two Are True!'
+  } else {
+    return 'Two Are Not True!'
+  };
+};
+
+twoAreTrue(4, 27);
+
+// Write a JavaScript program that runs when 1 of 2 things are true
+function oneIsTrue(arg1, arg2) {
+  if(arg1 || arg2) {
+    return 'One is True!'
+  } else {
+    return 'Not True!'
+  };
+};
+
+oneIsTrue(4, 0);
+
+// Write a JavaScript program that runs when both things are not true
+function noneAreTrue(arg1, arg2) {
+  if(arg1 && arg2) {
+    return 'Two Are True!'
+  } else {
+    return 'Two Are Not True!'
+  };
+};
+
+noneAreTrue(false, NaN);
