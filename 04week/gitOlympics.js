ada8fb8eef6cb29a325f3fe56520f6656e8d0819
Ty Sweet <tysweet77@gmail.com>
2018-02-06 19:22:27 -0600

    first commit

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let Olympians = ['Bruce Jenner', 'Mary-Lou Retton', 'Nadia', 'Jesse Owens', 'Michael Phelps'];
+
+const printListOfOlympians = (OlympiansList) => {
+  Olympians.forEach(function(OlympiansList) {
+    console.log(OlympiansList);
+  });
+}
+printListOfOlympians();
