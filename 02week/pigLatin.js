b6a2d34d9afb353a47952e0a80c7f8bca346336a
Ty Sweet <tysweet77@gmail.com>
2018-02-07 21:28:45 -0600

    so close, 2 out of 4 tests

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -10,63 +10,31 @@ const rl = readline.createInterface({
 
 function pigLatin(word) {
   let vowIndex = -1;
-  // const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
-  // const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n',
-  //   'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
-  // const firstLetterVowel = 'yay';
-  // const noFirstLetterVowel = 'ay';
-  // if (word.startsWith('a')) {
-  //   return word + firstLetterVowel;
-  // } else if (word.startsWith('e')) {
-  //   return word + firstLetterVowel;
-  // } else if (word.startsWith('i')) {
-  //   return word + firstLetterVowel;
-  // } else if (word.startsWith('o')) {
-  //   return word + firstLetterVowel;
-  // } else if (word.startsWith('u')) {
-  //   return word + firstLetterVowel;
-  // } else if (word.startsWith('y')) {
-  //   return word + firstLetterVowel;
-  // } else if (word.indexOf('a')){
-  //   return word.indexOf('a');
-  //   word + noFirstLetterVowel;
-  // } else if (word.indexOf('e')){
-  //   return word + noFirstLetterVowel;
-  // } else if (word.indexOf('i')){
-  //   return word + noFirstLetterVowel;
-  // } else if (word.indexOf('o')){
-  //   return word + noFirstLetterVowel;
-  // } else if (word.indexOf('u')){
-  //   return word + noFirstLetterVowel;
-  // } else if (word.indexOf('y')){
-  //   return word + noFirstLetterVowel;
-  // } else {
-  //   return 'nope';
-  // }
   if (word.indexOf('a') === 0 ||
       word.indexOf('e') === 0 ||
       word.indexOf('i') === 0 ||
       word.indexOf('o') === 0 ||
       word.indexOf('u') === 0) {
     return word + 'yay';
-  } else if (word.indexOf('a') > -1 && word.indexOf('a') > 0) {
+  }
+  if (word.indexOf('a') > 0 || word.indexOf('a') < vowIndex) {
     vowIndex = word.indexOf('a');
-  }
-  if (word.indexOf('e') > -1 && word.indexOf('e') < vowIndex) {
+    console.log(vowIndex);
+  } else if (word.indexOf('e') > 0 || word.indexOf('e') < vowIndex) {
     vowIndex = word.indexOf('e');
-  }
-  if (word.indexOf('i') > -1 && word.indexOf('i') < vowIndex) {
+    console.log(vowIndex);
+  } else if (word.indexOf('i') > 0 || word.indexOf('i') < vowIndex) {
     vowIndex = word.indexOf('i');
-  }
-  if (word.indexOf('o') > -1 && word.indexOf('o') < vowIndex) {
+    console.log(vowIndex);
+  } else if (word.indexOf('o') > 0 || word.indexOf('o') < vowIndex) {
     vowIndex = word.indexOf('o');
-  }
-  if (word.indexOf('u') > -1 && word.indexOf('u') < vowIndex) {
+    console.log(vowIndex);
+  } else if (word.indexOf('u') > 0 || word.indexOf('u') < vowIndex) {
     vowIndex = word.indexOf('u');
+    console.log(vowIndex);
   } else {
     return 'Input Valid Entry';
   }
-
 
   let firstPartOfWord = ''
   let secondPartOfWord = ''
