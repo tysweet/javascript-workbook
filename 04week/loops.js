596d2a45032412caf56cc7802c9236728d5cdaa1
Ty Sweet <tysweet77@gmail.com>
2018-02-15 08:39:27 -0600

    adding assignment

---
+++ 04week/loops.js
@@ -0,0 +1,34 @@
+'use strict'
+
+//for loop
+//Use a for loop to console.log each item in the array carsInReverse.
+
+
+//for...in loop
+//Create an object (an array with keys and values) called persons with the following data:
+//firstName: "Jane"
+// lastName: "Doe"
+// birthDate: "Jan 5, 1925"
+// gender: "female"
+// Use a for...in loop to console.log each key.
+// Then use a for...in loop and if state to console.log the value associated with the key birthDate.
+
+
+// while loop
+// Use a for loop to console.log the numbers 1 to 1000.
+
+
+// do...while loop
+// Use a do...while loop to console.log the numbers from 1 to 1000.
+
+
+// When is a for loop better than a while loop?
+
+
+// How is the readability of the code affected?
+
+
+// What is the difference between a for loop and a for...in loop?
+
+
+// What is the difference between a while loop and a do...while loop?
