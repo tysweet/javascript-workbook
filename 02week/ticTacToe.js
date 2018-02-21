ed4ce4c42dc95d8cab368718b0149d12c6ac86a4
Ty Sweet <tysweet77@gmail.com>
2018-02-21 14:37:42 -0600

    updated per comments

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -26,65 +26,68 @@ function printBoard() {
 }
 
 function horizontalWin() {
-  if ((board[0][0] === 'X') && (board[0][1] === 'X') && (board[0][2] === 'X')) {
+  if ((board[0][0] === playerTurn) &&
+  (board[0][1] === playerTurn) &&
+  (board[0][2] === playerTurn)) {
     return true;
-  } else if ((board[1][0] === 'X') && (board[1][1] === 'X') && (board[1][2] === 'X')) {
+  } else if ((board[1][0] === playerTurn) &&
+  (board[1][1] === playerTurn) &&
+  (board[1][2] === playerTurn)) {
     return true;
-  } else if ((board[2][0] === 'X') && (board[2][1] === 'X') && (board[2][2] === 'X')) {
-    return true;
-  } else if ((board[0][0] === 'O') && (board[0][1] === 'O') && (board[0][2] === 'O')) {
-    return true;
-  } else if ((board[1][0] === 'O') && (board[1][1] === 'O') && (board[1][2] === 'O')) {
-    return true;
-  } else if ((board[2][0] === 'O') && (board[2][1] === 'O') && (board[2][2] === 'O')) {
+  } else if ((board[2][0] === playerTurn) &&
+  (board[2][1] === playerTurn) &&
+  (board[2][2] === playerTurn)) {
     return true;
   }
 }
 
 
 function verticalWin() {
-  if ((board[0][0] === 'X') && (board[1][0] === 'X') && (board[2][0] === 'X')) {
+  if ((board[0][0] === playerTurn) &&
+  (board[1][0] === playerTurn) &&
+  (board[2][0] === playerTurn)) {
     return true;
-  } else if ((board[0][1] === 'X') && (board[1][1] === 'X') && (board[2][1] === 'X')) {
+  } else if ((board[0][1] === playerTurn) &&
+  (board[1][1] === playerTurn) &&
+  (board[2][1] === playerTurn)) {
     return true;
-  } else if ((board[0][2] === 'X') && (board[1][2] === 'X') && (board[2][2] === 'X')) {
-    return true;
-  } else if ((board[0][0] === 'O') && (board[1][0] === 'O') && (board[2][0] === 'O')) {
-    return true;
-  } else if ((board[0][1] === 'O') && (board[1][1] === 'O') && (board[2][1] === 'O')) {
-    return true;
-  } else if ((board[0][2] === 'O') && (board[1][2] === 'O') && (board[2][2] === 'O')) {
+  } else if ((board[0][2] === playerTurn) &&
+  (board[1][2] === playerTurn) &&
+  (board[2][2] === playerTurn)) {
     return true;
   }
 }
 
 function diagonalWin() {
-  if ((board[0][0] === 'X') && (board[1][1] === 'X') && (board[2][2] === 'X')) {
+  if ((board[0][0] === playerTurn) &&
+  (board[1][1] === playerTurn) &&
+  (board[2][2] === playerTurn)) {
     return true;
-  } else if ((board[0][2] === 'X') && (board[1][1] === 'X') && (board[2][0] === 'X')) {
-    return true;
-  } else if ((board[0][0] === 'O') && (board[1][1] === 'O') && (board[2][2] === 'O')) {
-    return true;
-  } else if ((board[0][2] === 'O') && (board[1][1] === 'O') && (board[2][2] === 'O')) {
+  } else if ((board[0][2] === playerTurn) &&
+  (board[1][1] === playerTurn) &&
+  (board[2][0] === playerTurn)) {
     return true;
   }
 }
 
 function checkForWin() {
-  horizontalWin();
-  verticalWin();
-  diagonalWin();
-  return true;
+  if ((horizontalWin()) ||
+  (verticalWin()) ||
+  (diagonalWin())) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function ticTacToe(row, column) {
+  checkForWin();
   board[row][column] = playerTurn;
   if (playerTurn === 'X') {
     (playerTurn = 'O');
   } else {
     (playerTurn = 'X');
   }
-  checkForWin();
 }
 
 function getPrompt() {
