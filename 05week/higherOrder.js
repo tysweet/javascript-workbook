122a0951e88e52232ca44d2ddbe8993d82a4dac3
Ty Sweet <tysweet77@gmail.com>
2018-02-23 09:09:50 -0600

    added project instructions

--- 05week/higherOrder.js
+++ 05week/higherOrder.js
@@ -1,1 +1,16 @@
 'use strict'
+
+// Create a forEach() function that takes an array of items and a function that
+// runs the function arr.length number of times.
+
+//code here
+
+// Create a map() function that takes an array of items and a function that
+// returns an array with each item manipulated by that function.
+
+//code here
+
+// Create a filter() function that takes an array of items and a function that
+// returns an array with only the items that return true in the function.
+
+//code here
