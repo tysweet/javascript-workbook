b7d0b22c4d6fc65f08f631322c57537487079d68
Ty Sweet <tysweet77@gmail.com>
2018-02-23 09:11:11 -0600

    added map function

--- 05week/higherOrder.js
+++ 05week/higherOrder.js
@@ -8,7 +8,15 @@
 // Create a map() function that takes an array of items and a function that
 // returns an array with each item manipulated by that function.
 
-//code here
+const myMap = (arr, callback) => {
+  const formatted =[];
+  arr.forEach((item) => {
+    callback(item);
+    formatted.push(callback(item));
+    console.log(callback(item));
+  });
+  return arr;
+};
 
 // Create a filter() function that takes an array of items and a function that
 // returns an array with only the items that return true in the function.
