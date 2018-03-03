3461b252686afcc920be5b51d4eb46c3cc417e10
Ty Sweet <tysweet77@gmail.com>
2018-03-03 12:13:46 -0600

    added boxes

--- 07week/react-tictactoe/src/App.js
+++ 07week/react-tictactoe/src/App.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
-import Board from './Board.js';
-import BoardRow from './boardRow.js';
-import Box from 'Box.js';
-import logo from './logo.svg';
+// import Board from './Board.js';
+// import BoardRow from './boardRow.js';
+// import Box from 'Box.js';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ]
+    };
+  }
   render() {
+    const rowStyle = {
+      backgroundColor: 'pink',
+      color: 'red',
+      height: '120px',
+      fontSize: '40px',
+      margin: '5px auto',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center'
+    };
+    const boxStyle = {
+      height: '110px',
+      width: '110px',
+      backgroundColor: 'pink',
+      margin: 'auto 20px',
+      border: '1px solid black'
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.state.board.map((row, index) => {
+          return <div key={index} style={rowStyle}>{row.map((box, index) => {
+            return <div key={index} style={boxStyle}></div>;
+          })}</div>;
+        })}
       </div>
     );
   }
 }
+
+
+
 
 export default App;
