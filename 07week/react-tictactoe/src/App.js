4a2082cd545d396f8e3f25b0f47e2a9610d4d1cd
Ty Sweet <tysweet77@gmail.com>
2018-03-06 18:04:09 -0600

    tictactoe

--- 07week/react-tictactoe/src/App.js
+++ 07week/react-tictactoe/src/App.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import Board from './Board.js';
 // import BoardRow from './boardRow.js';
 // import Box from 'Box.js';
 // import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       board: [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-      ]
+        [[null],[null],[null]],
+        [[null],['O'],[null]],
+        [[null],[null],[null]],
+      ],
+      playerTurn: true,
     };
+    console.log(this.state);
   }
+
+  handleClick(i) {
+    const board = this.state.board.slice();
+    board[i][i] = 'X';
+    this.setState({board: board});
+    // console.log(this);
+    // const box = this.state.board.slice();
+  //   this.setState({
+  //     box: box,
+  //   });
+  }
+
   render() {
+    // const status = 'Next player: ' + (this.state.playerTurn ? 'X' : 'O');
     const rowStyle = {
-      backgroundColor: 'pink',
+      backgroundColor: 'yellow',
       color: 'red',
       height: '120px',
       fontSize: '40px',
@@ -30,7 +45,7 @@ class App extends Component {
     const boxStyle = {
       height: '110px',
       width: '110px',
-      backgroundColor: 'pink',
+      backgroundColor: 'lightgrey',
       margin: 'auto 20px',
       border: '1px solid black'
     };
@@ -39,14 +54,15 @@ class App extends Component {
       <div>
         {this.state.board.map((row, index) => {
           return <div key={index} style={rowStyle}>{row.map((box, index) => {
-            return <div key={index} style={boxStyle}></div>;
+            return <div key={index} style={boxStyle}>
+              <button onClick={this.handleClick}></button>
+            </div>;
           })}</div>;
         })}
       </div>
     );
   }
 }
-
 
 
 
